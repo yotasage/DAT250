0fa676eadd04acf92bb5b266fb90b75b92cebe70
Jonathan Sæthre Ege <JonathanSaethreEge@outlook.com>
2020-09-12 02:29:57 +0200

    full-rework 1 of 2

--- app.py
+++ app.py
@@ -1,31 +1,44 @@
 # flask tutorial https://code.visualstudio.com/docs/python/tutorial-flask
+
+# To activate the virtual environment (venv) type in the following command in the terminal:
+# windows: & "e:/.../DAT250/Prosjekt/DAT250/.venv_win/Scripts/Activate.ps1"
+# mac: & "e:/.../DAT250/Prosjekt/DAT250/.venv_mac/Scripts/Activate.ps1"
+
+# The command line should look something like this when the venv is activated
+# windows: (.venv_win) PS E:\...\DAT250\Prosjekt\DAT250> 
+# mac: (.venv_mac) PS E:\...\DAT250\Prosjekt\DAT250> 
+
+# To run the server in vs code, type the following command in the terminal after activating the venv:
+# windows: python -m flask run
+# mac: python3 -m flask run
+
 # The development server looks for app.py by default. 
 
+# imports
 import datetime
 from flask import Flask
 from flask import render_template
 from flask import app
-app = Flask(__name__)
 
-# @app.route("/")
-# def home():
-#     return "Hello, Flask!"
+# something
+app = Flask(__name__)
 
 @app.route("/")
 def hello_there():
+    print("home")
     return render_template("index.html", date=datetime.datetime.now())
 
 @app.route("/pages/<page>")
 def pages(page = None):
-    print(page)
-    return render_template("/assets/" + page, date=datetime.datetime.now())
+    print(f"page = {page}")
+    return render_template("pages/" + page, date=datetime.datetime.now())
 
 @app.route("/assets/<asset>")
 def assets(asset = None):
-    print(asset)
-    return app.send_static_file("/assets/" + asset)
+    print(f"asset = {asset}")
+    return app.send_static_file("assets/" + asset)
 
 @app.route("/styles/<asset>")
-def styles(asset = None):
-    print(asset)
-    return app.send_static_file("/styles/" + asset)
+def styles(style = None):
+    print(f"style = {style}")
+    return app.send_static_file("style/" + style)
