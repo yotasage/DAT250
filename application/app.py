46442d7898224e192a42f2331d3a63b35166b8f0
Jonathan Sæthre Ege <JonathanSaethreEge@outlook.com>
2020-09-18 22:02:20 +0200

    Sending emails is now performed ona separate thread, improving server responsiveness.

--- application/app.py
+++ application/app.py
@@ -8,12 +8,15 @@ from flask_mail import Mail
 mail_settings = {
     "MAIL_SERVER": 'smtp.office365.com',
     "MAIL_PORT": 587,
-    "MAIL_USE_TLS": True,
-    "MAIL_USE_SSL": False,
-    "MAIL_USERNAME": os.environ.get('MAIL_USERNAME_FLASK'),         # $Env:MAIL_USERNAME_FLASK = "username" skrives inn i Powershell for windows for å sette epost addressen serveren skal sende mail fra
+    "MAIL_USE_TLS": True,                                           # Mail blir kryptert
+    "MAIL_USE_SSL": False,                                          # Dette er en annen krypterings greie vi ikke bruker
+    "MAIL_USERNAME": os.environ.get('MAIL_USERNAME_FLASK'),         # $Env:MAIL_USERNAME_FLASK = "username" skrives inn i Powershell for windows 
+                                                                    # for å sette epost addressen serveren skal sende mail fra, bruk studentmailen din, for eksempel studentnummer@uis.no
     "MAIL_DEFAULT_SENDER": os.environ.get('MAIL_USERNAME_FLASK'),   # Gjør at vi ikke trenger å oppgi hvem som sender eposten, dette er default
-    "MAIL_PASSWORD": os.environ.get('MAIL_PASSWORD_FLASK')          # $Env:MAIL_PASSWORD_FLASK = "password" skrives inn i Powershell for windows for å sette passorde til epost addressen serveren skal sende mail fra
-}                                                                   # Disse 2 tingene er noe vi absolutt ikke vil ha i kildekoden, altså skrevet inn her med tanke på at vi pusher dette til github
+    "MAIL_PASSWORD": os.environ.get('MAIL_PASSWORD_FLASK')          # $Env:MAIL_PASSWORD_FLASK = "password" skrives inn i Powershell for windows 
+                                                                    # for å sette passorde til epost addressen serveren skal sende mail fra, bruk feide passorde ditt
+}                                                                   # Disse 2 tingene (mail og passord, i hvert fall passord) er noe vi absolutt ikke vil ha (hardcoded) 
+                                                                    # i kildekoden, altså skrevet inn her med tanke på at vi pusher dette til github. Derfor bruker vi environment variabler
 
 # something
 app = Flask(__name__)
--- application/tools.py
+++ application/tools.py
@@ -1,12 +1,16 @@
 import os
-from app import app, mail
+import datetime
+import threading
+# import time  # For testing
+from flask import copy_current_request_context
 from flask_mail import Message as _Message
 
-DEFAULT_RECIPIENTS = ["the.jona.mr@outlook.com"]  #, "232765@uis.no", "250623@uis.no", "250631@uis.no"]
+from app import app, mail
+
+DEFAULT_RECIPIENTS = ["email@domain.com"]  # Dette er ei liste over alle default mottakere av mailen, hver mottaker skilles med komma
 DOMAIN_NAME = 'dinnettbank.tk'
 DEFAULT_MESSAGE_SUBJECT = "Flask test email, sent from server as " + os.environ.get('MAIL_USERNAME_FLASK')
 TEST_BODY="text body"
-TEST_HTML = '<b>Test mail from the Flask server of ' + DOMAIN_NAME + ' </b><br> <br> This mail is sent from ' + os.environ.get('MAIL_USERNAME_FLASK') + ', who is a representative of ' + DOMAIN_NAME
 
 
 class Message(_Message):
@@ -20,16 +24,38 @@ class Message(_Message):
             self.msgId = self.msgId.split('@')[0] + DOMAIN_NAME + '>'  # Denne korter ned message ID slik at Office365 godtar mailen.
 
 
-def send_mail(sender=None, recipients=DEFAULT_RECIPIENTS, subject=DEFAULT_MESSAGE_SUBJECT, body=TEST_BODY, html=TEST_HTML):
+def test_html_constructor():
+    TEST_HTML = (   '<b>Test mail from the Flask server of ' + DOMAIN_NAME + 
+                    ' </b><br> <br> This mail is sent from ' + os.environ.get('MAIL_USERNAME_FLASK') + 
+                    ', who is a representative of ' + DOMAIN_NAME + 
+                    '<br><br> This mail was sent ' + str(datetime.datetime.now())) 
+    return TEST_HTML
+
+
+def send_mail(sender=None, recipients=DEFAULT_RECIPIENTS, subject=DEFAULT_MESSAGE_SUBJECT, body=TEST_BODY, html=None):
+
+    # This function is run in a separate thread to send emails.
+    # This will reduce the load on the main thread/program so that the responsiveness of the server is maintained.
+    # Sending a mail typically took 1 second for the send_mail function, now it takes 0.001 seconds typically. Used time.time() to find the time spent on sending the email
+    @copy_current_request_context
+    def mail_sender_thread(msg):
+        mail.send(msg)
+
     # Konstruer melding
     if sender is None:
-        msg = Message(subject=subject, recipients=recipients)  # Use MAIL_DEFAULT_SENDER
+        msg = Message(subject=subject, recipients=recipients)  # Use MAIL_DEFAULT_SENDER as sender
     else:
         msg = Message(subject=subject, sender=sender, recipients=recipients)
 
-    # These two options can also be included within the parenthesis when making the message above
+    # The following two options (body and html) can also be included within the parenthesis when making the message above
     msg.body = body  # Don't know what this is
-    msg.html = html  # This is what we see when we view that mail using HTML
+
+    if html is None:
+        msg.html = test_html_constructor()
+    else:
+        msg.html = html  # This is what we see when we view the mail using HTML
 
     # Send melding
-    mail.send(msg)
+    mail_sending_thread = threading.Thread(target=mail_sender_thread, args=(msg,))
+    mail_sending_thread.start()
+
