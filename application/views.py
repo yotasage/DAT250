2c329ab72105f1a0aaaf308ed8c4e7ce234c3c57
Jonathan SE <JonathanSaethreEge@outlook.com>
2020-09-23 11:40:19 +0200

    noe mer kommentering, forberedelse til å realisere registrering av bruker, generel optimalisering

--- application/views.py
+++ application/views.py
@@ -13,18 +13,25 @@ from tools import send_mail
 def index():
     return render_template("index.html", date=datetime.datetime.now(), username="Vebjørn")
 
+# Denne er bare for GET forespørsler.
 @app.route("/pages/login.html", methods=['GET'])
 def login(page = None):
-    messages = request.args.get('error')
+    messages = request.args.get('error')  # Henter argumentet error fra URL som kommer med forespørselen fra nettleseren til brukeren.
     return render_template("pages/login.html", date=datetime.datetime.now(), error=messages)
 
 @app.route("/pages/<page>", methods=['GET'])
 def pages(page = None):
-    return render_template("pages/" + page, date=datetime.datetime.now(), error=None)
+    return render_template("pages/" + page, date=datetime.datetime.now())
 
 @app.route("/assets/<asset>")
 def assets(asset = None):
     return app.send_static_file("assets/" + asset)
+
+# Må teste om denne gjør noe
+@app.route("/favicon.ico")
+def favicon():
+    print("FAVICON FUNKSJON")
+    return app.send_static_file("favicon.png")
 
 @app.route("/styles/<asset>")
 def styles(style = None):
