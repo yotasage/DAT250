bf777536dcabaef9d56b9037f4f148950649027d
GjerdeV <gjerde.martin@hotmail.com>
2020-10-06 18:06:50 +0200

    added more to skey

--- application/testauth.py
+++ application/testauth.py
@@ -1,5 +1,5 @@
 # Bruk denne filen for å sjekke hva TOTP-en er til enhver tid
 
 import pyotp
-totp = pyotp.TOTP("YSEBRNQCNKWTEHIF")
+totp = pyotp.TOTP("FX7OI3AS7VBGDDTKMOYZ3Z6KRQ6JQF66CNCUJQAJRKGFB7Y7")
 print("Current OTP:", totp.now())
