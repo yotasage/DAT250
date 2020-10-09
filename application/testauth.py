22bf2134f7bc5c09cb5948dcaafb32291ab26a09
GjerdeV <gjerde.martin@hotmail.com>
2020-10-09 14:02:54 +0200

    .

--- application/testauth.py
+++ application/testauth.py
@@ -6,6 +6,6 @@ import pyotp
 
 secret_key, img = generate_QR(navn, 12)
 print(secret_key)
-totp = pyotp.TOTP(secret_key)
-print("Current OTP:", totp.now())
+totp = pyotp.TOTP(secret_key).now()
+print("Current OTP:", totp)
 img.show()
