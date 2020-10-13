2a0ec5eb2ef1723a5c6ea913537a963f97804b0d
GjerdeV <gjerde.martin@hotmail.com>
2020-10-13 14:44:38 +0200

    skjult site_key

--- application/tools.py
+++ application/tools.py
@@ -112,7 +112,7 @@ def is_human(captcha_response):
     """ Validating recaptcha response from google server
         Returns True captcha test passed for submitted form else returns False.
     """
-    secret = "6LeVXtYZAAAAAEucQFkxkIpYskRfAyKB140yp-HF"
+    secret = os.environ.get('SITE_KEY')
     payload = {'response':captcha_response, 'secret':secret}
     response = requests.post("https://www.google.com/recaptcha/api/siteverify", payload)
     response_text = json.loads(response.text)
