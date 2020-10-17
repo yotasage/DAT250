e8074da6ec1c1ea0d156ff6d4b14a440d692dae3
GjerdeV <gjerde.martin@hotmail.com>
2020-10-17 14:54:54 +0200

    skjult encryption key

--- application/models.py
+++ application/models.py
@@ -1,9 +1,10 @@
 from app import db
+import os
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_utils import EncryptedType
 from sqlalchemy_utils.types.encrypted.encrypted_type import AesEngine
 
-key = 'secretkey1234'
+key = os.environ.get("ENCRYPTION_KEY")
 
 # Med kryptering:
 class User(db.Model):
@@ -18,13 +19,13 @@ class User(db.Model):
     city = db.Column(EncryptedType(db.String, key, AesEngine, 'pkcs5'), unique=False, nullable=False)
     postcode = db.Column(EncryptedType(db.Integer, key, AesEngine, 'oneandzeroes'), unique=False, nullable=False)
     address = db.Column(EncryptedType(db.String, key, AesEngine, 'pkcs5'), unique=False, nullable=False)
-    hashed_password = db.Column(EncryptedType(db.String, key, AesEngine, 'pkcs5'), unique=False, nullable=True) # Funker ikke å kryptere hashed_password, får feilmeldingen --> TypeError: a bytes-like object is required, not 'str'
+    hashed_password = db.Column(EncryptedType(db.String, key, AesEngine, 'pkcs5'), unique=False, nullable=True)
     salt = db.Column(EncryptedType(db.String, key, AesEngine, 'pkcs5'), unique=False, nullable=True)
     verification_code = db.Column(EncryptedType(db.String, key, AesEngine, 'pkcs5'), unique=True, nullable=True)
     verified = db.Column(EncryptedType(db.Boolean, key, AesEngine, 'zeroes'), unique=False, nullable=False)
     password_reset_code = db.Column(EncryptedType(db.String, key, AesEngine, 'pkcs5'), unique=True, nullable=True)
     secret_key = db.Column(EncryptedType(db.String, key, AesEngine, 'pkcs5'), unique=True, nullable=True)
-    failed_logins = db.Column(EncryptedType(db.Integer, key, AesEngine, 'oneandzeroes'), unique=False, nullable=False) # Funker ikke å kryptere failed_logins, tror kanskje det er fordi den er 0?
+    failed_logins = db.Column(EncryptedType(db.Integer, key, AesEngine, 'oneandzeroes'), unique=False, nullable=False)
     blocked_login_until = db.Column(EncryptedType(db.String, key, AesEngine, 'pkcs5'), unique=False, nullable=True)
     last_password_reset_request = db.Column(EncryptedType(db.String, key, AesEngine, 'pkcs5'), unique=False, nullable=False)
 
